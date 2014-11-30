4d317d63a6cc0bb34a975caf97f1223f41a67e2d
pjaspers <piet@jaspe.rs>
2014-12-01 00:26:58 +0100

    Testing the configuration to stamp.

--- test/middleware_test.rb
+++ test/middleware_test.rb
@@ -28,6 +28,13 @@ describe Shack::Middleware do
       @middleware = Shack::Middleware.new(@app, @sha)
     end
 
+    after do
+      Shack::Middleware.configure do |shack|
+        shack.sha = nil
+        shack.content = nil
+      end
+    end
+
     it "doesn't break the original app" do
       status, _, _ = @middleware.call(fake_env("http://something.com"))
       assert_equal status, 200
@@ -54,12 +61,30 @@ describe Shack::Middleware do
       assert_equal Shack::Middleware.sha, "O'Sullivan"
     end
 
+    it "favors the initialized sha over the configured one" do
+      Shack::Middleware.configure { |s| s.sha = "O'Sullivan" }
+      app = ->(_) { [200, { "Content-Type" => "text/html" }, fake_page] }
+      s = Shack::Middleware.new(app, "The Rocket")
+      assert_equal s.sha, "The Rocket"
+    end
+
     it "can set a custom content string" do
       string = "O'Sullivan - http://bbc.co.uk/{{sha}}"
       Shack::Middleware.configure do |s|
         s.content = string
       end
       assert_equal Shack::Middleware.content, string
+    end
+
+    it "sets the stamp with the configuration" do
+      app = ->(_) { [200, { "Content-Type" => "text/html" }, fake_page] }
+      Shack::Middleware.configure do |s|
+        s.sha = "abc123"
+        s.content = "Ronnie The Rocket - {{sha}}"
+      end
+      middleware = Shack::Middleware.new(app)
+      _, _, response = middleware.call(fake_env("http://something.com"))
+      assert_match(/Ronnie The Rocket - abc123/, response.body.first)
     end
   end
   def fake_page
