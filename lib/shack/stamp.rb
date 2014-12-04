ab7ee7247af86f81500fe4d4975dc7ecdfd62d61
pjaspers <piet@jaspe.rs>
2014-12-04 23:19:53 +0100

    Using CSS specifity and not !important
    This to make sure the text is white, if the thing contains an <a href>
    and something sets the color, it will still be off.

--- lib/shack/stamp.rb
+++ lib/shack/stamp.rb
@@ -27,11 +27,23 @@ module Shack
       end
     end
 
+    def stamp_style
+      {
+        position: "fixed",
+        bottom: 0,
+        right: 0,
+        height: "16px",
+        background: "rgb(0, 0, 0) transparent",
+        "background-color" => "rgba(0, 0, 0, 0.6)",
+        padding: "0 5 x"
+      }.collect { |k, v| "%s: %s;" % [k, v] }.join(" ")
+    end
+
     def html
       <<HTML
-<div id="sha-stamp" style="position: fixed; bottom: 0; right: 0; height: 16px; background-color: darkred; padding: 0 5px;">
-  <span style="text-align: center; color: white; font-weight: normal;font-size: 12px !important;">
-    <small>#{content}</small>
+<div id="sha-stamp" style="#{stamp_style}">
+  <span style="text-align: center;">
+    <small style="color: white; font-weight: normal;font-size: 12px;">#{content}</small>
   </span>
 </div>
 HTML
