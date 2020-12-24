7cee1798a627755bc9080d1ee42e20c5b9558c37
pjaspers <piet@pjaspers.com>
2020-12-24 22:02:49 +0100

    Remove pry

--- test/helper.rb
+++ test/helper.rb
@@ -3,7 +3,6 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "mocha"
-require "pry"
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
